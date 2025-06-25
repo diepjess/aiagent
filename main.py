15d162cb165a9b70a4403ea5b77146a5b3493c84
diepjess <jessicadiep97@gmail.com>
2025-06-25 15:35:39 -0400

    fix: user prompt now takes multiple arguments.

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ from dotenv import load_dotenv
 from google import genai
 
 def main():
-    if len(sys.argv) != 2:
-        print('Usage: python3 main.py "<your_prompt_here>"')
-        sys.exit(1)
     load_dotenv()
+    
+    args = sys.args[1:]
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python3 main.py "your prompt here"')
+        print('Example: python3 main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+    
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    prompt = sys.argv[1]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=prompt
+        contents=user_prompt,
     )
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
