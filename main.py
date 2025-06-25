8e03cb63d5cfd946ad7f68a58d12603692273999
diepjess <jessicadiep97@gmail.com>
2025-06-25 16:01:27 -0400

    feat: Add option to use verbose tag

--- main.py
+++ main.py
@@ -8,31 +8,39 @@ from google.genai import types
 def main():
     load_dotenv()
     
-    args = sys.argv[1:]
+    is_verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python3 main.py "your prompt here"')
+        print('\nUsage: python3 main.py "your prompt here" [--verbose]')
         print('Example: python3 main.py "How do I build a calculator app?"')
         sys.exit(1)
-    user_prompt = " ".join(args)
-    
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     user_prompt = " ".join(args)
     
+    if is_verbose:
+        print(f"User prompt: {user_prompt}")
+    
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages)
+    generate_content(client, messages, is_verbose)
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, is_verbose):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
     )
+    if is_verbose:
+        prompt_token = response.usage_metadata.prompt_token_count
+        response_token = response.usage_metadata.candidates_token_count
+        print(f"Prompt tokens: {prompt_token}")
+        print(f"Response tokens: {response_token}")
     print("Response:")
     print(response.text)
 
