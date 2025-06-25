b5f9f9799ca9da75fa6db3e19ef3b899b6b72c59
diepjess <jessicadiep97@gmail.com>
2025-06-25 15:44:31 -0400

    feat: barebone implementation to accept multiple prompts

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import os, sys
+import sys
+import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 def main():
     load_dotenv()
     
-    args = sys.args[1:]
+    args = sys.argv[1:]
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python3 main.py "your prompt here"')
@@ -16,12 +19,20 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    user_prompt = " ".join(args)
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=user_prompt,
+        contents=messages,
     )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
