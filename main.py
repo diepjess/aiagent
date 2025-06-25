a9d1f6953b7365bcb0ad5da3da1b8c78ee260cd1
diepjess <jessicadiep97@gmail.com>
2025-06-25 15:31:19 -0400

    feat: barebones implementation of AI agent

---
+++ main.py
@@ -0,0 +1,24 @@
+import os, sys
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py "<your_prompt_here>"')
+        sys.exit(1)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    prompt = sys.argv[1]
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=prompt
+    )
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
