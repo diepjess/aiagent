3b37507dacc1cb5ee1347685a2cc8b15db6812e7
diepjess <jessicadiep97@gmail.com>
2025-06-26 15:18:23 -0400

    feat: add write_file function and relevant tests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
     
 def test_get_files_info():
@@ -52,5 +53,18 @@ def test_get_file_content():
     print(result)
 
 
+def test_write_file():
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print('Result from "lorem.txt')
+    print(result)
+    
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print('Result from "pkg/morelorem.txt')
+    print(result)
+    
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print('Result from "/tmp/temp.txt"')
+    print(result)
+
 if __name__ == "__main__":
-    test_get_file_content()
+    test_write_file()
