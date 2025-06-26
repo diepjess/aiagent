eaf5838d5a3060d8261b23013e224d645b4e2458
diepjess <jessicadiep97@gmail.com>
2025-06-26 17:30:40 -0400

    feat: add function declaration for write_file

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 
 def write_file(working_directory, file_path, content):
@@ -19,3 +20,18 @@ def write_file(working_directory, file_path, content):
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return f'Error: {e}'
+
+
+schema_write_file = types.FunctionDeclaration(
+        name="write_file",
+        description="Write or overwrite files, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "write": types.Schema(
+                    type=types.Type.STRING,
+                    description="Write or overwrite files, relative to the working directory.",
+                ),
+            },
+        ),
+    )
