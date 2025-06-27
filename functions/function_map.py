e9c945c818173ab58defef8e09441dfe517e36d9
diepjess <jessicadiep97@gmail.com>
2025-06-27 16:44:35 -0400

    feat: add barebone implementation to allow AI agent to call functions. add WORKING_DIR to config. add hard coded map of function names to function

---
+++ functions/function_map.py
@@ -0,0 +1,12 @@
+from get_file_content import get_file_content
+from get_files_info import get_files_info
+from run_python import run_python_file
+from write_file import write_file
+
+
+function_map = {
+    "get_file_content": get_file_content,
+    "get_file_info": get_files_info,
+    "run_python_file": run_python_file,
+    "write_file": write_file,
+}
