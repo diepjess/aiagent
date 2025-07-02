8df5804d01136c7436a7329539ca8774f1ea0e1d
diepjess <jessicadiep97@gmail.com>
2025-07-02 17:51:33 -0400

    fix: moved loop outside of function

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 MAX_CHARS = 10000
 WORKING_DIR = "./calculator"
-LOOP_LIMIT = 20
+MAX_ITERS = 20
 
 MODEL_NAME = "gemini-2.0-flash-001"
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google import genai
 from google.genai import types
 
 from config import MODEL_NAME
-from config import LOOP_LIMIT
+from config import MAX_ITERS
 from prompts import SYSTEM_PROMPT
 from call_function import available_functions
 from call_function import call_function
@@ -33,45 +33,65 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-
-    generate_content(client, messages, is_verbose)
+    
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
+        
+        try:
+            final_response = generate_content(client, messages, is_verbose)
+            if final_response:
+                print("Final response")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
 
 
 def generate_content(client, messages, is_verbose):
-    for i in range(LOOP_LIMIT):
-        response = client.models.generate_content(
-            model=MODEL_NAME,
-            contents=messages,
-            config=types.GenerateContentConfig(
-                tools = [available_functions],
-                system_instruction=SYSTEM_PROMPT,
-            ),
-        )
-        for candidate in response.candidates:
-            message = candidate.content
-            messages.append(message)
-            print(f"{i + 1}: {message}")
-        if not response.function_calls:
-            print("Response:")
-            print(response.text)
-            break
-        function_responses = []
-        for function_call_part in response.function_calls:
-            function_call_result = call_function(function_call_part, verbose=is_verbose)
-            try:
-                function_call_response = function_call_result.parts[0].function_response.response
-            except (AttributeError, IndexError):
-                raise Exception("Function call did not return a valid response.")
-            messages.append(function_call_result)
-            if is_verbose:
-                print(f"-> {function_call_response}")
-            function_responses.append(function_call_result.parts[0])
-
+    response = client.models.generate_content(
+        model=MODEL_NAME,
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools = [available_functions],
+            system_instruction=SYSTEM_PROMPT,
+        ),
+    )
     if is_verbose:
         prompt_token = response.usage_metadata.prompt_token_count
         response_token = response.usage_metadata.candidates_token_count
         print(f"Prompt tokens: {prompt_token}")
         print(f"Response tokens: {response_token}")
+    function_calls = []
+    if response.candidates:
+        for candidate in response.candidates:
+            function_call_content = candidate.content
+            messages.append(function_call_content)
+            if function_call_content.parts[0].function_call:
+                function_calls.append(function_call_content)
+    
+    if not function_calls:
+        return response.text
+    
+    function_responses = []
+    for function_call_part in function_calls:
+        function_call_result = call_function(function_call_part.parts[0].function_call, is_verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("Error: empty function call result")
+        if is_verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+    
+    if not function_responses:
+        raise Exception("Error: no function responsed generated, exiting")
+    
+    messages.append(types.Content(role="tool", parts=function_responses))
 
 
 if __name__ == "__main__":
